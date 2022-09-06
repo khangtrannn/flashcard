2d8a12a505e72a445393edd31e081a1ff5592ee0
Khang Tran <khangtrann8198@gmail.com>
2022-09-06 23:22:38 +0700

    Init flashcard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlashcardComponent } from './components/flashcard/flashcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlashcardComponent,
   ],
   imports: [
     BrowserModule,
