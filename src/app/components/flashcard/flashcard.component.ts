5dd332996662da95b84556dc295ac9af784a8202
Khang Tran <khangtrann8198@gmail.com>
2023-02-07 18:46:25 +0700

    Update

--- src/app/components/flashcard/flashcard.component.ts
+++ src/app/components/flashcard/flashcard.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-flashcard',
   templateUrl: './flashcard.component.html',
-  styleUrls: ['./flashcard.component.scss']
+  styleUrls: ['./flashcard.component.scss'],
 })
-export class FlashcardComponent implements OnInit {
+export class FlashcardComponent {
+  readonly KEY = {
+    space: ' ',
+    f: 'f',
+  };
 
-  constructor() { }
+  shouldFlip = false;
 
-  ngOnInit() {
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.key === this.KEY.f) {
+      this.shouldFlip = !this.shouldFlip;
+    }
   }
-
 }
