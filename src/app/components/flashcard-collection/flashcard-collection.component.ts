4aace96a8dcb7958ca03525ec1d7fea2a2a9b059
Khang Tran <khangtrann8198@gmail.com>
2023-04-21 16:09:03 +0700

    Refactor flashcard collection

--- src/app/components/flashcard-collection/flashcard-collection.component.ts
+++ src/app/components/flashcard-collection/flashcard-collection.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import {
+  Flashcard,
+  FlashcardService,
+} from 'src/app/services/flashcard.service';
 
 @Component({
   selector: 'app-flashcard-collection',
   templateUrl: './flashcard-collection.component.html',
-  styleUrls: ['./flashcard-collection.component.scss']
+  styleUrls: ['./flashcard-collection.component.scss'],
 })
-export class FlashcardCollectionComponent implements OnInit {
+export class FlashcardCollectionComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  flashcards: Flashcard[] | undefined;
 
-  constructor() { }
+  constructor(private flashcardService: FlashcardService) {}
 
   ngOnInit(): void {
+    this.flashcardService
+      .getAll()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((flashcards) => {
+        this.flashcards = flashcards;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
